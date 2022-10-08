1b3411f996ad15aa3b2a28c0dac586a40c0992ca
Misha Surikov <mishkasurikov@gmail.com>
2022-10-08 20:37:57 +0500

    Lesson4

---
+++ Lesson4/HomeworkLesson4/HomeworkLesson4.cs
@@ -0,0 +1,81 @@
+﻿void FillArray(int[] num)
+{
+    Random random = new Random();
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = random.Next(-100, 101);
+    }
+}
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + " ");
+    }
+}
+void Task1()
+{
+    // Используя определение степени числа, напишите цикл, который
+    // принимает на вход два числа и возводит число А в степень В.
+    Console.WriteLine("Введте число А: ");
+    int number_a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введте число B: ");
+    int number_b = Convert.ToInt32(Console.ReadLine());
+    int degree = 1;
+    for (int i = 1; i <= number_b; i++)
+    {
+        degree = degree * number_a;
+    }
+    Console.WriteLine($"Число {number_a} в степени {number_b}: {degree}");
+}
+void Task2()
+{
+    //Напишите программу, которая принимает на вход число
+    // и выдает сумму цифр в числе
+    Console.WriteLine("Введте число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int number2 = number;
+    int sum = 0;
+    while (number != 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    Console.WriteLine($"Сумма цифр в числе {number2}: {sum}");
+}
+void Task3()
+{
+    // Напишите программу, которая задает массив из 8 случайных целых чисел и выводит 
+    // отсортированный по модулю массив
+    Console.WriteLine("Введите длину массива: ");
+    int array_size = Convert.ToInt32(Console.ReadLine());
+    int[] numbers = new int[array_size];
+    FillArray(numbers);
+    PrintArray(numbers);
+    while (array_size > 1)
+    {
+        int max = Math.Abs(numbers[0]);
+        int i = 0;
+        int max_index = 0;
+        while (i < array_size)
+        {
+            if (Math.Abs(numbers[i]) >= max)
+            {
+                max = Math.Abs(numbers[i]);
+                max_index = i;
+
+            }
+            i = i + 1;
+        }
+        int current_number = numbers[array_size - 1];
+        numbers[array_size - 1] = numbers[max_index];
+        numbers[max_index] = current_number;
+        array_size = array_size - 1;
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Отсортированный массив:");
+    PrintArray(numbers);
+}
+Task1();
+Task2();
+Task3();
---
+++ Lesson4/WebinarLesson4/WebinarLesson4.cs
@@ -0,0 +1,72 @@
+﻿void Zadacha24()
+{
+    // Задача 24: Напишите программу, которая
+    // принимает на вход число (А) и выдаёт сумму чисел
+    // от 1 до А.
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine(sum);
+}
+
+void Zadacha26()
+{
+    //     Решение в группах задач:
+    // Задача 26: Напишите программу, которая принимает
+    // на вход число и выдаёт количество цифр в числе.
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        sum += 1;
+    }
+    Console.WriteLine(sum);
+}
+void Zadacha28()
+{
+    // Задача 28: Напишите программу, которая
+    // принимает на вход число N и выдаёт
+    // произведение чисел от 1 до N.
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int sum = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        sum = sum * i;
+    }
+    Console.WriteLine(sum);
+}
+void Zadacha30()
+{
+    //     Задача 30: Напишите программу, которая
+    // выводит массив из 8 элементов, заполненный
+    // нулями и единицами в случайном порядке.
+    int size = 8;
+    int[] numb = new int[size];
+    FillArray(numb, 0, 1);
+    PrintArray(numb);
+}
+void FillArray(int[] numbers, int minValue = 0, int maxValue = 100)
+{
+    maxValue++;
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(minValue, maxValue);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine("");
+}
+Zadacha30();
