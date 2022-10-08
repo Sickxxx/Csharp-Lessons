d2342c53d2ddf9bb8c584f5e0ef6c43c8408c1f9
Misha Surikov <mishkasurikov@gmail.com>
2022-10-08 16:33:01 +0500

    Lesson2

--- Lesson2/HomeworkLesson2/HomeworkLesson2.cs
+++ Lesson2/HomeworkLesson2/HomeworkLesson2.cs
@@ -1,79 +1,44 @@
-﻿int InputNumber()
+﻿void Task10()
 {
-    Console.WriteLine("Введите число:");
-    return Convert.ToInt32(Console.ReadLine());
-}
-void Task10()
-{
-    //Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, 
-    //является ли оно палиндромом..
-    int num = InputNumber();
-    while (99999 < num || num < 9999)
-    {
-        Console.WriteLine("Число не пятизначное, введите пятизначное число");
-        num = InputNumber();
-    }
-    if (num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10)
-    {
-        Console.WriteLine("Число является палиндромом");
-    }
-    else Console.WriteLine("Число не является палиндромом");
+    //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+    //на выходе показывает вторую цифру этого числа.
+    Console.WriteLine("Введите трехзначное число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Вторая цифра в числе:" + (num / 10) % 10);
 
 
 }
-
 void Task13()
 {
-    //Задача 21 Напишите программу, которая принимает на вход
-    //координаты двух точек и находит расстояние между ними в 3D пространстве.
-    Console.WriteLine("Введите координаты точки A:");
-    Console.Write("x1: ");
-    int x1 = InputNumber();
-    Console.Write("y1: ");
-    int y1 = InputNumber();
-    Console.Write("z1: ");
-    int z1 = InputNumber();
-    Console.WriteLine("Введите координаты точки B:");
-    Console.Write("x2: ");
-    int x2 = InputNumber();
-    Console.Write("y2: ");
-    int y2 = InputNumber();
-    Console.Write("z2: ");
-    int z2 = InputNumber();
-    Console.WriteLine($"Точки:A({x1},{y1},{z1}), B({x2},{y2},{z2})");
-    Console.WriteLine($"Расстояние между точками A и B: {Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2))}");
+    //Задача 13: Напишите программу, которая с помощью деления выводит третью цифру
+    // заданного числа или сообщает, что третьей цифры нет.
+    Console.WriteLine("");
+    Console.WriteLine("Введите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int num = Math.Abs(number);
+    if (num % 100 == num) Console.WriteLine("Третьей цифры нет");
+    else
+    {
+        while (num / 10 > 100)
+        {
+            num = num / 10;
 
+        }
+        Console.WriteLine("Третья цифра в числе: " + num % 10);
+    }
 }
 
 void Task15()
 {
-    //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-    int num = InputNumber();
-    int k = 1;
-    double qube;
-    Console.WriteLine("Таблица квадратов для числа " + num);
-    if (num > 0)
-    {
-        while (k <= num)
-        {
-            qube = Math.Pow(k, 3);
-            Console.WriteLine($"{k}\t{qube}");
-            k++;
-        }
-    }
-    else if (num < 0)
-    {
-        while (k >= num)
-        {
-            qube = Math.Pow(k, 3);
-            Console.WriteLine($"{k}\t{qube}");
-            k--;
-        }
-    }
-    else Console.WriteLine(num);
-
+    //Задача 15: Напишите программу, которая принимает на вход цифру, 
+    //обозначающую день недели, и проверяет, является ли этот день выходным.
+    Console.WriteLine("");
+    Console.WriteLine("Введите число от 1 до 7 ми:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num == 6 || num == 7) Console.WriteLine("Выходной");
+    else Console.WriteLine("НЕ выходной");
 }
 
-Task10();
+//Task10();
 Task13();
-Task15();
+//Task15();
