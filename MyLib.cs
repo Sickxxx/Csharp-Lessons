8870a8b5acd16e348e49a129796b53724d7bce09
Misha Surikov <mishkasurikov@gmail.com>
2022-10-09 18:59:14 +0500

    Update Mylib

--- MyLib.cs
+++ MyLib.cs
@@ -20,6 +20,23 @@ namespace MyLib
             }
             Console.WriteLine("");
         }
+        public static void FillArray(double[] numbers)
+        {
+            maxValue++;
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Math.Round(random.NextDouble * 200 - 100, 2);
+            }
+        }
+        public static void PrintArray(double[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine("");
+        }
         public static void ReverseArray(int[] numbers)
         {
             int maxIndex = numbers.Length - 1;
@@ -57,5 +74,50 @@ namespace MyLib
                 Console.WriteLine();
             }
         }
+        public static void FillArray(double[,] arr)
+        {
+            Random random = new Random();
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arr[i, j] = Math.Round(random.NextDouble() * 100, 1);
+                }
+            }
+        }
+        public static void PrintArray(double[,] arr)
+        {
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void AverageColumns(int[,] arr)
+        {
+            Console.Write("Среднее арифметическое каждого столбца: ");
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int j = 0; j < rows; j++)
+            {
+                double count = 0;
+                double sum = 0;
+                for (int i = 0; i < columns; i++)
+                {
+                    sum = sum + arr[i, j];
+                    count += 1;
+                }
+                double average = Math.Round(sum / count, 2);
+                Console.Write(average + ";  ");
+            }
+        }
+
     }
 }
