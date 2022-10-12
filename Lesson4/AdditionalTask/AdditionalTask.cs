b5d4607caaa8c6b050946ebc63d0e7c72358fb68
Misha Surikov <mishkasurikov@gmail.com>
2022-10-12 15:37:55 +0500

    Lesson4

--- Lesson4/AdditionalTask/AdditionalTask.cs
+++ Lesson4/AdditionalTask/AdditionalTask.cs
@@ -1,16 +1,99 @@
-﻿// Задача 1. На вход подаётся натуральное десятичное число. Проверьте, является ли оно палиндромом в двоичной записи.
+﻿void Zadacha1()
+{// Задача 1. На вход подаётся натуральное десятичное число. Проверьте, является ли оно палиндромом в двоичной записи.
+    Console.WriteLine("Введите число от 1 до 9");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string result = "";
+    while (number > 0)
+    {
+        result = number % 2 + result;
+        number /= 2;
+    }
+    Console.WriteLine(result);
+    int result1 = Convert.ToInt32(result);
+    if (result1 > 1 && result1 < 100)
+    {
+        if (result1 / 10 == result1 % 10) Console.WriteLine($"число {number} в двоичной системе {result1} является палиндромом");
+        else Console.WriteLine($"число {number} в двоичной системе {result1} не является палиндромом");
+    }
+    if (result1 >= 100 && result1 < 1000)
+    {
+        if (result1 / 100 == result1 % 10) Console.WriteLine($"число {number} в двоичной системе {result1} является палиндромом");
+        else Console.WriteLine($"число {number} в двоичной системе {result1} не является палиндромом");
+    }
+    if (result1 >= 1000 && result1 < 10000)
+    {
+        if (result1 / 1000 == result1 % 10 && result1 / 100 % 10 == result1 / 10 % 10) Console.WriteLine($"число {number} в двоичной системе {result1} является палиндромом");
+        else Console.WriteLine($"число {number} в двоичной системе {result1} не является палиндромом");
+    }
 
-// Задача 2. Напишите метод, который заполняет массив случайным количеством (от 1 до 100) нулей и единиц. Размер массива должен совпадать с квадратом количества единиц в нём.
+}
+void Zadacha2()
+{
+    // Задача 2. Напишите метод, который заполняет массив случайным количеством (от 1 до 100) нулей и единиц. 
+    //Размер массива должен совпадать с квадратом количества единиц в нём.
 
-// Задача 3. Массив на 100 элементов задаётся случайными числами от 1 до 99. Определите самый часто встречающийся элемент в массиве. Если таких элементов несколько, вывести их все.
+    int[] numbers = new int[100];
+}
+void Zadacha3()
+{
+    // Задача 3. Массив на 100 элементов задаётся случайными числами от 1 до 99. 
+    //Определите самый часто встречающийся элемент в массиве. Если таких элементов несколько, вывести их все.
+    int size = 50;
+    int[] numbers = new int[size];
+    MyLib.Array.FillArray(numbers, 1, 20);
+    MyLib.Array.PrintArray(numbers);
+    int max = 0;
+    int maxind = 0;
+    bool flag = false;
+    for (int i = 0; i < size; i++)
+    {
+        if (numbers[i] == numbers[i = 1])
+        {
+            flag = true;
+            maxind = numbers[i];
+        }
+        
+    }
+    Console.WriteLine($"Самый часто встречающийся элемент {maxind}");
+
+}
+void FindValue(int[] numbers, int value)
+{
+    bool flag = false;
+    // for (int i = 0; i < numbers.Length; i++)
+    // {
+    //     if (numbers[i] == value)
+    //     {
+    //         flag = !flag;
+    //     }
+    // }
+    int i = 0;
+    while (!flag && i < numbers.Length)
+    {
+        if (value == numbers[i])
+        {
+            flag = true;
+        }
+        i++;
+    }
+
+    if (flag)
+    {
+        Console.WriteLine($"Число {value}  находится в массиве");
+    }
+    else
+    {
+        Console.WriteLine($"Число {value} Не находится в массиве");
+    }
 
 
-// Создание игры: 
+    // Создание игры: 
 
-// Задача 1*. С помощью команд очистки консоли и печати реализовать игру-бродилку в консоли.
-// https://docs.microsoft.com/ru-ru/dotnet/api/system.console.clear?view=net-6.0
-// https://docs.microsoft.com/ru-ru/dotnet/api/system.console.setcursorposition?view=net-6.0
-// https://docs.microsoft.com/ru-ru/dotnet/api/system.console.readkey?view=net-6.0
-// https://docs.microsoft.com/ru-ru/dotnet/api/system.consolekey?view=net-6.0
+    // Задача 1*. С помощью команд очистки консоли и печати реализовать игру-бродилку в консоли.
+    // https://docs.microsoft.com/ru-ru/dotnet/api/system.console.clear?view=net-6.0
+    // https://docs.microsoft.com/ru-ru/dotnet/api/system.console.setcursorposition?view=net-6.0
+    // https://docs.microsoft.com/ru-ru/dotnet/api/system.console.readkey?view=net-6.0
+    // https://docs.microsoft.com/ru-ru/dotnet/api/system.consolekey?view=net-6.0
 
-// Задача 2*. Создать игру лабиринт.
+    // Задача 2*. Создать игру лабиринт.
+    Zadacha3();
---
+++ Lesson4/AdditionalTask/MyLib.cs
@@ -0,0 +1,122 @@
+namespace MyLib
+{
+    class Array
+    {
+
+        public static void FillArray(int[] numbers, int minValue = 0, int maxValue = 100)
+        {
+            maxValue++;
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minValue, maxValue);
+            }
+        }
+        public static void PrintArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine("");
+        }
+        public static void FillArray(double[] numbers)
+        {
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Math.Round(random.NextDouble() * 200 - 100, 2);
+            }
+        }
+        public static void PrintArray(double[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine("");
+        }
+        public static void ReverseArray(int[] numbers)
+        {
+            int maxIndex = numbers.Length - 1;
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                (numbers[i], numbers[maxIndex - i]) = (numbers[maxIndex - i], numbers[i]);
+            }
+        }
+    }
+    class ArrayMD
+    {
+        public static void FillArray(int[,] arr, int minValue = 0, int maxValue = 100)
+        {
+            Random random = new Random();
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arr[i, j] = random.Next(minValue, maxValue);
+                }
+            }
+        }
+        public static void PrintArray(int[,] arr)
+        {
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void FillArray(double[,] arr)
+        {
+            Random random = new Random();
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arr[i, j] = Math.Round(random.NextDouble() * 100, 1);
+                }
+            }
+        }
+        public static void PrintArray(double[,] arr)
+        {
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void AverageColumns(int[,] arr)
+        {
+            Console.Write("Среднее арифметическое каждого столбца: ");
+            int rows = arr.GetLength(0);
+            int columns = arr.GetLength(1);
+            for (int j = 0; j < rows; j++)
+            {
+                double count = 0;
+                double sum = 0;
+                for (int i = 0; i < columns; i++)
+                {
+                    sum = sum + arr[i, j];
+                    count += 1;
+                }
+                double average = Math.Round(sum / count, 2);
+                Console.Write(average + ";  ");
+            }
+        }
+
+    }
+}
