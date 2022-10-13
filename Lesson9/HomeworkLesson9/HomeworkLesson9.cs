7fb2dafd8567311db6ab5218cf3a9fa32c70b2ff
Misha Surikov <mishkasurikov@gmail.com>
2022-10-14 01:44:54 +0500

    Lesson9

---
+++ Lesson9/HomeworkLesson9/HomeworkLesson9.cs
@@ -0,0 +1,73 @@
+﻿void Zadacha64()
+{
+    //     Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+    // в промежутке от N до 1. Выполнить с помощью рекурсии.
+    Console.WriteLine("Введите число N: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Numbers(num);
+}
+void Numbers(int num, int i = 1)
+{
+    Console.Write(num + "  ");
+    num--;
+    if (num < i)
+    {
+        return;
+    }
+    Numbers(num, i);
+}
+void Zadacha66()
+{
+    // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+    // натуральных элементов в промежутке от M до N.
+
+    // M = 1; N = 15 -> 120
+    // M = 4; N = 8. -> 30
+    Console.WriteLine("Введите число M: ");
+    int num_m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N: ");
+    int num_n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Сумма элементов от M до N " + SumNumbers(num_m, num_n));
+}
+int SumNumbers(int num_m, int num_n, int sum = 0)
+{
+    sum = sum + num_m;
+    num_m += 1;
+    if (num_m > num_n)
+    {
+        return sum;
+    }
+    return SumNumbers(num_m, num_n, sum);
+}
+void Zadacha68()
+{
+    // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+    // Даны два неотрицательных числа m и n.
+    // m = 2, n = 3 -> A(m,n) = 9
+    // m = 3, n = 2 -> A(m,n) = 29
+
+    Console.WriteLine("Введите число M: ");
+    int num_m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N: ");
+    int num_n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(AckermanFunc(num_m, num_n));
+}
+int AckermanFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AckermanFunc(m - 1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return AckermanFunc(m - 1, AckermanFunc(m, n - 1));
+    }
+    return AckermanFunc(m,n);
+}
+Zadacha64();
+Zadacha66();
+Zadacha68();
---
+++ Lesson9/WebinarLesson9/WebinarLesson9.cs
@@ -0,0 +1,73 @@
+﻿void Zadacha63()
+{
+    //     Задача 63: Задайте значение N. Напишите программу, которая выведет
+    // все натуральные числа в промежутке от 1 до N.
+    Console.WriteLine("Введите число N: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Numbers(num);
+}
+void Numbers(int num, int i = 1)
+{
+    Console.Write(i + "  ");
+    i++;
+    if (i > num)
+    {
+        return;
+    }
+    Numbers(num, i);
+}
+void Zadacha65()
+{
+    //     Задача 65: Задайте значения M и N. Напишите программу, которая
+    // выведет все натуральные числа в промежутке от M до N.
+    Console.WriteLine("Введите число M: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Numbers(num, num2);
+}
+void Zadacha67()
+{
+    //     Задача 67: Напишите программу, которая будет принимать на вход число и
+    // возвращать сумму его цифр.
+    Console.WriteLine("Введите число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(SumNumbers(num));
+
+}
+int SumNumbers(int num, int sum = 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+    if (num == 0)
+    {
+        return sum;
+    }
+    return SumNumbers(num, sum);
+}
+void Zadacha69()
+{
+    //     Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+    // возводит число А в целую степень B с помощью рекурсии.
+    Console.WriteLine("Введите число A: ");
+    int num_a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число B: ");
+    int num_b = Convert.ToInt32(Console.ReadLine());
+    Power(num_a, num_b);
+
+}
+void Power(int num_a, int num_b, int i = 0, int result = 1)
+{
+    if (i >= num_b)
+    {
+        Console.WriteLine($"{num_a}^{num_b} = {result}");
+        return;
+    }
+    result *= num_a;
+    i++;
+    Power(num_a, num_b, i, result);
+
+
+}
+
+Zadacha69();
